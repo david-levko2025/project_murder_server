e3f5fd82f2e27af61efca86de0a67feb7e68c247
david <mdavidl@gmail.com>
2026-01-06 17:10:30 +0200

    config and .env

---
+++ app/core/config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings
+
+class Setting(BaseSettings):
+    MYSQL_USER: str
+    MYSQL_PORT:int
+    MYSQL_PASSWORD:str
+    MYSQL_HOST:str
+    MYSQL_DB:str
+
+
+setting = Setting()
